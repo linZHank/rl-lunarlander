59ffb57e75ca4c9a8f228fd932713b71b0ec06ab
linzhank <linzhank@gmail.com>
2020-04-20 08:37:32 -0400

    initiate ppo

--- train_ppo.py
+++ train_ppo.py
@@ -25,7 +25,7 @@ if gpus:
         print(e)
 
 import logging
-logging.basicConfig(format='%(asctime)s %(message)s',level=logging.INFO)
+logging.basicConfig(format='%(asctime)s %(message)s',level=logging.DEBUG)
 
 
 # Create LunarLander env
@@ -35,9 +35,9 @@ env = gym.make('LunarLander-v2')
 actor_net = tf.keras.Sequential(
     [
         tf.keras.layers.InputLayer(input_shape=env.observation_space.shape),
-        tf.keras.layers.Dense(128, activation='relu'),
-        tf.keras.layers.Dense(128, activation='relu'),
-        tf.keras.layers.Dense(env.action_space.n, activation='softmax')
+        tf.keras.layers.Dense(128, activation='tanh'),
+        tf.keras.layers.Dense(128, activation='tanh'),
+        tf.keras.layers.Dense(env.action_space.n)
     ]
 )
 actor_net.summary()
@@ -52,6 +52,26 @@ critic_net = tf.keras.Sequential(
     ]
 )
 critic_net.summary()
+
+def compute_loss_actor(batch_obs, batch_logprobs, batch_acts):
+    tensor_obs = tf.convert_to_tensor(batch_obs)
+    # print(tensor_obs)
+    tensor_logprobs = tf.convert_to_tensor(batch_logprobs)
+    # print(tensor_logprobs)
+    tensor_new_samples = tf.squeeze(tf.random.categorical(tensor_logprobs, num_samples=1))
+    # print(tensor_new_samples)
+    tensor_logpi = tf.math.multiply(tensor_logprobs, tf.one_hot(tensor_new_samples, depth=env.action_space.n))
+    # print(tensor_logpi)
+    tensor_reduce_logpi = tf.math.reduce_sum(tensor_logpi, axis=1)
+    # print(tensor_reduce_logpi)
+    tensor_acts = tf.convert_to_tensor(batch_acts)
+    tensor_logpi_old = tf.math.multiply(tensor_logprobs, tf.one_hot(tensor_acts, depth=env.action_space.n))
+    tensor_reduce_logpi_old = tf.math.reduce_sum(tensor_logpi_old, axis=1)
+    # print(tensor_reduce_logpi_old)
+    tensor_ratio = tf.math.exp(tensor_reduce_logpi - tensor_reduce_logpi_old)
+    print(tensor_ratio)
+
+
 
 def grad_actor(actor_net, batch_obs, batch_acts, batch_rtgs, batch_dones, batch_next_obs):
     with tf.GradientTape() as tape:
@@ -85,68 +105,14 @@ def reward_to_go(rews, gamma):
         rtgs[i] = rews[i] + (gamma*rtgs[i+1] if i+1 < n else 0)
     return rtgs
 
+def compute_advantage():
+    pass
 
-################################### Test Loss ###################################
-# gamma = 0.99
-# obs = env.reset()
-# batch_obs = []
-# batch_acts = []
-# batch_dones = []
-# # batch_rets = []
-# batch_rtgs = []
-# batch_next_obs = []
-# rewards = []
-# for ep in range(4):
-#     for st in range(100):
-#         prob_acts = actor_net(obs.reshape(1,-1))
-#         action = np.squeeze(tf.random.categorical(logits=prob_acts, num_samples=1))
-#         next_obs, rew, done, _ = env.step(action)
-#         batch_obs.append(obs.copy())
-#         batch_acts.append(action)
-#         batch_dones.append(done)
-#         batch_next_obs.append(next_obs)
-#         rewards.append(np.float32(gamma**st*rew))
-#         obs = next_obs.copy()
-#     batch_rtgs += list(reward_to_go(rewards))
-#     rewards = []
-#     obs = env.reset()
-#
-# # ep_ret, ep_len = np.sum(ep_rewards), len(ep_rewards)
-# # batch_rets += [ep_ret]*ep_len
-# # compute loss
-# batch_prob_acts = actor_net(tf.convert_to_tensor(batch_obs))
-# # print(batch_prob_acts) # debug
-# batch_acts_onehot = tf.one_hot(tf.convert_to_tensor(batch_acts), depth=env.action_space.n)
-# # print(batch_acts_onehot) # debug
-# batch_pis = tf.math.multiply(batch_prob_acts, batch_acts_onehot)
-# # print(batch_pis) # debug
-# batch_reduce_pis = tf.math.reduce_sum(batch_pis, axis=1)
-# # print(batch_reduce_pis) # debug
-# batch_logpis = tf.math.log(batch_reduce_pis)
-# # print(batch_logpis) # debug
-# # print(tf.convert_to_tensor(batch_rets)) # debug
-# batch_vals = tf.squeeze(critic_net(tf.convert_to_tensor(batch_obs)))
-# # print(batch_vals)
-# batch_next_vals = tf.squeeze(critic_net(tf.convert_to_tensor(batch_next_obs)))
-# # print(batch_next_vals)
-# # print(tf.convert_to_tensor(batch_rtgs))
-# batch_advs = tf.convert_to_tensor(batch_rtgs) + (1.-np.array(batch_dones))*gamma*batch_next_vals - batch_vals
-# # print(batch_advs)
-# pg_loss = -tf.math.reduce_mean(tf.math.multiply(batch_logpis, batch_advs))
-# # print(pg_loss)
-# vals_pred = tf.squeeze(critic_net(tf.convert_to_tensor(batch_obs)))
-# # print(vals_pred)
-# vals_target = tf.convert_to_tensor(batch_rtgs)
-# # print(vals_target)
-# loss_critic = tf.keras.losses.MSE(vals_target, vals_pred)
-# # print(loss_critic)
-
-##################################################################################
 
 
 # params
-num_batches = 256
-batch_size = 8192
+num_batches = 2 #56
+batch_size = 16 #8192
 lr_actor = 3e-5
 lr_critic = 5e-3
 gamma = 0.99
@@ -163,47 +129,52 @@ episode = 0
 step = 0
 for s in range(num_batches):
     batch_obs = []
+    batch_logprobs = []
     batch_acts = []
-    batch_drtgs = [] # discounted reward to go
+    batch_rets = [] # discounted reward to go
     batch_dones = []
     batch_next_obs = []
+    batch_advs = []
     rewards = []
     while True:
-        # env.render()
-        prob_a = actor_net(obs.reshape(1,-1))
-        # print(prob_a) # debug
-        action = np.squeeze(tf.random.categorical(logits=prob_a, num_samples=1)) # squeeze (1,1) to (1,)
+        env.render()
+        logprob = tf.nn.log_softmax(actor_net(obs.reshape(1,-1))) # shape=(1, action_space.n)
+        # print(logprob) # debug
+        action = np.squeeze(tf.random.categorical(logits=logprob, num_samples=1)) # squeeze (1,1) to (1,)
         # print(action) # debug
         next_obs, rew, done, info = env.step(action)
         batch_obs.append(obs.copy())
+        batch_logprobs.append(tf.squeeze(logprob))
         batch_acts.append(action)
         rewards.append(np.float32(rew))
         batch_dones.append(done)
         batch_next_obs.append(next_obs.copy())
+        # update obs
+        obs = next_obs.copy() # Critical
         step += 1
         # print(obs, rew, done, info)
         logging.debug("\n-\nbatch: {}, episode: {}, step: {}, batch length: {} \naction: {} \nobs: {}".format(batch+1, episode+1, step+1, len(batch_obs), action, obs))
-        obs = next_obs.copy()
         if done:
             episode += 1
             step = 0
             ep_rets.append(sum(rewards))
             ave_rets.append(sum(ep_rets)/len(ep_rets))
-            batch_drtgs += list(reward_to_go(rewards, gamma))
-            logging.info("\n---\nbatch: {}, episode: {} \nreturn: {} \n".format(batch+1, episode+1, ep_rets[-1]))
+            # batch_drtgs += list(reward_to_go(rewards, gamma))
+            logging.info("\n---\nbatch: {}, episode: {} \nreturn: {} \n".format(batch+1, episode, ep_rets[-1]))
             obs, done, rewards = env.reset(), False, []
             if len(batch_obs) > batch_size:
                 batch += 1
                 break
-    loss_actor, grads_actor = grad_actor(actor_net, batch_obs, batch_acts, batch_drtgs, batch_dones, batch_next_obs)
-    optimizer_actor.apply_gradients(zip(grads_actor, actor_net.trainable_variables))
-    for i in range(val_train_iters):
-        loss_critic, grads_critic = grad_critic(critic_net, batch_obs, batch_drtgs)
-        optimizer_critic.apply_gradients(zip(grads_critic, critic_net.trainable_variables))
-
-    logging.info("\n====\nbatch: {}, episode: {} \nloss_actor: {}, loss_critic: {} \nmean return: {} \n====\n".format(batch, episode, loss_actor, loss_critic, np.mean(batch_drtgs)))
-
-fig, ax = plt.subplots(figsize=(8, 6))
-fig.suptitle('Averaged Returns')
-ax.plot(ave_rets)
-plt.show()
+    compute_loss_actor(batch_obs, batch_logprobs, batch_acts)
+#     loss_actor, grads_actor = grad_actor(actor_net, batch_obs, batch_acts, batch_drtgs, batch_dones, batch_next_obs)
+#     optimizer_actor.apply_gradients(zip(grads_actor, actor_net.trainable_variables))
+#     for i in range(val_train_iters):
+#         loss_critic, grads_critic = grad_critic(critic_net, batch_obs, batch_drtgs)
+#         optimizer_critic.apply_gradients(zip(grads_critic, critic_net.trainable_variables))
+#
+#     logging.info("\n====\nbatch: {}, episode: {} \nloss_actor: {}, loss_critic: {} \nmean return: {} \n====\n".format(batch, episode, loss_actor, loss_critic, np.mean(batch_drtgs)))
+#
+# fig, ax = plt.subplots(figsize=(8, 6))
+# fig.suptitle('Averaged Returns')
+# ax.plot(ave_rets)
+# plt.show()
