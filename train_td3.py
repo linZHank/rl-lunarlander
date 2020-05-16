f4c2641263c1e561d37cd83dcbb9d246875b9d21
linzhank <linzhank@gmail.com>
2020-05-16 02:59:39 -0400

    prototype td3

--- train_td3.py
+++ train_td3.py
@@ -40,57 +40,101 @@ logging.basicConfig(format='%(asctime)s %(message)s',level=logging.DEBUG)
 
 ################################################################
 """
-Build actor_net, critic_net and replay_buffer
+instantiate env
 """
-def mlp(sizes, activation, output_activation=None):
-    inputs = tf.keras.Input(shape=(sizes[0],))
-    x = tf.keras.layers.Dense(sizes[1], activation=activation)(inputs)
-    for i in range(2,len(sizes)-1):
-        x = tf.keras.layers.Dense(sizes[i], activation=activation)(x)
-    outputs = tf.keras.layers.Dense(sizes[-1], activation=output_activation)(x)
+env = gym.make('LunarLanderContinuous-v2')
+################################################################
 
-    return tf.keras.Model(inputs=inputs, outputs=outputs)
 
-class MLPActor(tf.Module):
-    def __init__(self, obs_dim, act_dim, hidden_sizes, activation, act_limit):
-        super().__init__()
-        pi_sizes = [obs_dim] +list(hidden_sizes) + [act_dim]
-        assert len(pi_sizes)>=3
-        self.pi = mlp(sizes=pi_sizes, activation=activation, output_activation='tanh')
-        # self.pi_targ = tf.keras.models.clone_model(self.pi)
-        self.act_limit = act_limit
+################################################################
+"""
+Build actor_net, critic_net
+"""
+# def mlp(sizes, activation, output_activation=None):
+#     inputs = tf.keras.Input(shape=(sizes[0],))
+#     x = tf.keras.layers.Dense(sizes[1], activation=activation)(inputs)
+#     for i in range(2,len(sizes)-1):
+#         x = tf.keras.layers.Dense(sizes[i], activation=activation)(x)
+#     outputs = tf.keras.layers.Dense(sizes[-1], activation=output_activation)(x)
+#
+#     return tf.keras.Model(inputs=inputs, outputs=outputs)
+#
+# class MLPActor(tf.Module):
+#     def __init__(self, obs_dim, act_dim, hidden_sizes, activation, act_limit):
+#         super().__init__()
+#         pi_sizes = [obs_dim] +list(hidden_sizes) + [act_dim]
+#         assert len(pi_sizes)>=3
+#         self.pi = mlp(sizes=pi_sizes, activation=activation, output_activation='tanh')
+#         # self.pi_targ = tf.keras.models.clone_model(self.pi)
+#         self.act_limit = act_limit
+#
+#     @tf.Module.with_name_scope
+#     def __call__(self, obs):
+#         return self.act_limit * tf.squeeze(self.pi(obs))
+#
+# class MLPQFunction(tf.Module):
+#     def __init__(self, obs_dim, act_dim, hidden_sizes, activation):
+#         super().__init__()
+#         q_sizes = [obs_dim+act_dim] +list(hidden_sizes) + [1]
+#         self.q = mlp(sizes=q_sizes, activation=activation)
+#
+#     @tf.Module.with_name_scope
+#     def __call__(self, obs, act):
+#         q = self.q(tf.concat([obs, act], axis=-1))
+#         return tf.squeeze(q) # Critical to ensure q has right shape.
+#
+# class MLPActorCritic(tf.Module):
+#     def __init__(self, obs_dim, act_dim, act_limit, hidden_sizes=(256,256),
+#                  activation='relu'):
+#         super().__init__()
+#         # obs_dim = observation_space.shape[0]
+#         # act_dim = action_space.shape[0]
+#         # act_limit = action_space.high[0]
+#         # build policy and value functions
+#         self.pi = MLPActor(obs_dim, act_dim, hidden_sizes, activation, act_limit)
+#         self.q1 = MLPQFunction(obs_dim, act_dim, hidden_sizes, activation)
+#         self.q2 = MLPQFunction(obs_dim, act_dim, hidden_sizes, activation)
+#
+#     def act(self, obs):
+#         a = tf.stop_gradient(self.pi(obs).numpy())
+#         return np.squeeze(a)
+actor_net = tf.keras.Sequential(
+    [
+        tf.keras.layers.InputLayer(input_shape=env.observation_space.shape),
+        tf.keras.layers.Dense(256, activation='relu'),
+        tf.keras.layers.Dense(256, activation='relu'),
+        tf.keras.layers.Dense(env.action_space.shape[0], activation='tanh')
+    ]
+)
+actor_net_targ = tf.keras.models.clone_model(actor_net)
 
-    @tf.Module.with_name_scope
-    def __call__(self, obs):
-        return self.act_limit * tf.squeeze(self.pi(obs))
+critic_net_1 = tf.keras.Sequential(
+    [
+        tf.keras.layers.InputLayer(input_shape=(env.observation_space.shape[0]+env.action_space.shape[0])),
+        tf.keras.layers.Dense(256, activation='relu'),
+        tf.keras.layers.Dense(256, activation='relu'),
+        tf.keras.layers.Dense(1, activation='linear')
+    ]
+)
+critic_net_1_targ = tf.keras.models.clone_model(critic_net_1)
 
-class MLPQFunction(tf.Module):
-    def __init__(self, obs_dim, act_dim, hidden_sizes, activation):
-        super().__init__()
-        q_sizes = [obs_dim+act_dim] +list(hidden_sizes) + [1]
-        self.q = mlp(sizes=q_sizes, activation=activation)
+critic_net_2 = tf.keras.Sequential(
+    [
+        tf.keras.layers.InputLayer(input_shape=(env.observation_space.shape[0]+env.action_space.shape[0])),
+        tf.keras.layers.Dense(256, activation='relu'),
+        tf.keras.layers.Dense(256, activation='relu'),
+        tf.keras.layers.Dense(1, activation='linear')
+    ]
+)
+critic_net_2_targ = tf.keras.models.clone_model(critic_net_2)
 
-    @tf.Module.with_name_scope
-    def __call__(self, obs, act):
-        q = self.q(tf.concat([obs, act], axis=-1))
-        return tf.squeeze(q) # Critical to ensure q has right shape.
+################################################################
 
-class MLPActorCritic(tf.Module):
-    def __init__(self, obs_dim, act_dim, act_limit, hidden_sizes=(256,256),
-                 activation='relu'):
-        super().__init__()
-        # obs_dim = observation_space.shape[0]
-        # act_dim = action_space.shape[0]
-        # act_limit = action_space.high[0]
-        # build policy and value functions
-        self.pi = MLPActor(obs_dim, act_dim, hidden_sizes, activation, act_limit)
-        self.q1 = MLPQFunction(obs_dim, act_dim, hidden_sizes, activation)
-        self.q2 = MLPQFunction(obs_dim, act_dim, hidden_sizes, activation)
 
-    def act(self, obs):
-        a = tf.stop_gradient(self.pi(obs).numpy())
-        return np.squeeze(a)
-
+################################################################
+"""
+Build replay_buffer
+"""
 class ReplayBuffer:
     """
     A simple FIFO experience replay buffer for TD3 agents.
@@ -127,14 +171,55 @@ class ReplayBuffer:
 """
 Compute loss and gradients
 """
-def compute_loss_q(ac, data):
-    obs, act, rew, next_obs, done = data['obs'], data['act'], data['rew'], data['next_obs'], data['done']
-    q1 = ac.q1(obs, act)
-    q2 = ac.q2(obs, act)
-    pass
+def compute_q_loss(act_limit, target_noise, noise_clip, batch):
+    obs_batch, act_batch, rew_batch, next_obs_batch, done_batch = batch['obs'], batch['act'], batch['rew'], batch['next_obs'], batch['done']
+    q1 = critic_net_1(tf.concat([obs_batch, act_batch], axis=1))
+    q2 = critic_net_2(tf.concat([obs_batch, act_batch], axis=1))
+    pi_targ = tf.stop_gradient(tf.math.multiply(act_limit, actor_net_targ(obs_batch)))
+    # print("pi_targ: {}".format(pi_targ))
+    # target policy smoothing
+    epsilon = tf.stop_gradient(tf.random.normal(pi_targ.shape)*target_noise)
+    epsilon = tf.clip_by_value(epsilon, -noise_clip, noise_clip)
+    next_act = pi_targ + epsilon
+    next_act = tf.clip_by_value(next_act, -act_limit, act_limit)
+    # targe Q-values
+    next_q1 = tf.stop_gradient(critic_net_1_targ(tf.concat([next_obs_batch, next_act], axis=1)))
+    next_q2 = tf.stop_gradient(critic_net_2_targ(tf.concat([next_obs_batch, next_act], axis=1)))
+    pessimistic_next_q = tf.math.minimum(next_q1, next_q2)
+    q_targ = rew_batch + gamma*(1 - done_batch)*pessimistic_next_q
+    # MSE loss
+    loss_q1 = tf.keras.losses.MSE(q_targ, q1)
+    loss_q2 = tf.keras.losses.MSE(q_targ, q2)
+    loss_Q = loss_q1 + loss_q2
 
-def update(data, timer):
-    pass
+    return loss_Q
+
+def compute_q1_grads(act_limit, target_noise, noise_clip, batch):
+    with tf.GradientTape() as tape:
+        loss_Q = compute_q_loss(act_limit, target_noise, noise_clip, batch)
+    grads_q1 = tape.gradient(loss_Q, critic_net_1.trainable_variables)
+    q1_optimizer.apply_gradients(zip(grads_q1, critic_net_1.trainable_variables))
+
+def compute_q2_grads(act_limit, target_noise, noise_clip, batch):
+    with tf.GradientTape() as tape:
+        loss_Q = compute_q_loss(act_limit, target_noise, noise_clip, batch)
+    grads_q2 = tape.gradient(loss_Q, critic_net_2.trainable_variables)
+    q2_optimizer.apply_gradients(zip(grads_q2, critic_net_2.trainable_variables))
+
+def compute_pi_loss(act_limit, batch):
+    obs_batch = batch['obs']
+    pi = tf.math.multiply(act_limit, actor_net(obs_batch))
+    objective_pi = critic_net_1(tf.concat([obs_batch, pi], axis=1))
+    loss_pi = -tf.math.reduce_mean(objective_pi)
+
+    return loss_pi
+
+def compute_pi_grads(act_limit, batch):
+    with tf.GradientTape() as tape:
+        loss_pi = compute_pi_loss(act_limit, batch)
+    grads_pi = tape.gradient(loss_pi, actor_net.trainable_variables)
+    actor_optimizer.apply_gradients(zip(grads_pi, actor_net.trainable_variables))
+
 # # @tf.function
 # def compute_loss_critic(critic_net, critic_net_stable, actor_net_stable, batch_reps):
 #     # compute predict Q
@@ -203,22 +288,17 @@ def update(data, timer):
 """
 Main
 """
-def get_action(self, obs, noise_scale):
-    a = self.act(obs)
-    a += noise_scale * np.random.randn(act_dim)
-    return np.clip(a, -act_limit, act_limit)
-
 # params
-steps_per_epoch=400
-epochs=10
+steps_per_epoch=4000
+epochs=100
 replay_size=int(1e6)
 gamma=0.99
 polyak=0.995
 pi_lr=1e-3
 q_lr=1e-3
 batch_size=100
-warmup_steps=100
-update_after=100
+warmup_steps=1000
+update_after=1000
 update_freq=50
 act_noise=0.1
 target_noise=0.2
@@ -227,16 +307,14 @@ policy_delay=2
 num_test_episodes=10
 # max_ep_len=1000
 # instantiate env, actor-critic net and replay_buffer
-env = gym.make('LunarLanderContinuous-v2')
 obs_dim = env.observation_space.shape[0]
 act_dim = env.action_space.shape[0]
 act_limit = env.action_space.high
-ac = MLPActorCritic(obs_dim, act_dim, act_limit)
-# ac_targ = deepcopy(ac)
 replay_buffer = ReplayBuffer(obs_dim=obs_dim, act_dim=act_dim, size=replay_size)
 # Create Optimizer
-optimizer_critic = tf.keras.optimizers.Adam(learning_rate=q_lr)
-optimizer_actor = tf.keras.optimizers.Adam(learning_rate=q_lr)
+q1_optimizer = tf.keras.optimizers.Adam(learning_rate=q_lr)
+q2_optimizer = tf.keras.optimizers.Adam(learning_rate=q_lr)
+actor_optimizer = tf.keras.optimizers.Adam(learning_rate=pi_lr)
 model_dir = './training_models/ddpg/'+datetime.now().strftime("%Y-%m-%d-%H-%M")
 if not os.path.exists(model_dir):
     os.makedirs(model_dir)
@@ -244,14 +322,14 @@ episodic_returns, episodic_lengths = [], []
 total_steps = steps_per_epoch * epochs
 start_time = time.time()
 obs, ep_ret, ep_len = env.reset(), 0, 0
+episode = 0
 # start
 for t in range(total_steps):
     # take random actions until warmup steps elapsed; add noise to policy generated action after that
     if t > warmup_steps:
-        a = ac.act(obs.reshape(1,-1)).copy()
+        a = tf.stop_gradient(tf.squeeze(actor_net(obs.reshape(1,-1))))
         a += act_noise * np.random.randn(act_dim)
         act = np.clip(a, -act_limit, act_limit)
-        # act = ac.get_action(obs.reshape(1,-1), act_noise)
     else:
         act = env.action_space.sample()
     # step
@@ -265,21 +343,78 @@ for t in range(total_steps):
     obs = next_obs.copy()
     # end of trajectory handling
     if done or (ep_len>=env.spec.max_episode_steps):
+        episode += 1
         episodic_returns.append(ep_ret)
         episodic_lengths.append(ep_len)
+        logging.info(
+            "\n================================================================\nEpisode: {}, EpisodeLength: {} \nTotalSteps: {} \nEpReturns: {} \nAveEpReturn: {} \nTime: {} \n================================================================\n".format(
+                episode,
+                ep_len,
+                t+1,
+                ep_ret,
+                ep_ret/ep_len,
+                time.time()-start_time
+            )
+        )
         obs, ep_ret, ep_len = env.reset(), 0, 0
     # let's update neural nets
     if t>=update_after and not t%update_freq:
         for i in range(update_freq):
             batch = replay_buffer.sample_batch(batch_size)
-            update(data=batch, timer=i)
-    # end of epoch handling
-    if not (t+1)%steps_per_epoch:
-        epoch = (t+1)//steps_per_epoch
-        # save model
-        pass
-
-
+            # loss_Q = compute_q_loss(act_limit, target_noise, noise_clip, batch)
+            # print("loss_Q: {}".format(loss_Q))
+            # loss_pi = compute_pi_loss(act_limit, batch)
+            # print("loss_pi: {}".format(loss_pi))
+            compute_q1_grads(act_limit, target_noise, noise_clip, batch)
+            compute_q2_grads(act_limit, target_noise, noise_clip, batch)
+            if not i%policy_delay:
+                compute_pi_grads(act_limit, batch)
+                # polyak averaging
+                weights_critic1_update = []
+                for w_q1, w_q1_targ in zip(critic_net_1.get_weights(), critic_net_1_targ.get_weights()):
+                    w_q1_upd = polyak*w_q1_targ
+                    w_q1_upd = w_q1_upd + (1 - polyak)*w_q1
+                    weights_critic1_update.append(w_q1_upd)
+                critic_net_1_targ.set_weights(weights_critic1_update)
+                weights_critic2_update = []
+                for w_q2, w_q2_targ in zip(critic_net_2.get_weights(), critic_net_2_targ.get_weights()):
+                    w_q2_upd = polyak*w_q2_targ
+                    w_q2_upd = w_q2_upd + (1 - polyak)*w_q2
+                    weights_critic2_update.append(w_q2_upd)
+                critic_net_2_targ.set_weights(weights_critic2_update)
+                weights_actor_update = []
+                for w_pi, w_pi_targ in zip(actor_net.get_weights(), actor_net_targ.get_weights()):
+                    w_pi_upd = polyak*w_pi_targ
+                    w_pi_upd = w_pi_upd + (1 - polyak)*w_pi
+                    weights_actor_update.append(w_pi_upd)
+                actor_net_targ.set_weights(weights_actor_update)
+#     # end of epoch handling
+#     if not (t+1)%steps_per_epoch:
+#         epoch = (t+1)//steps_per_epoch
+#         # save model
+#         pass
+#
+# Test trained model
+input("Press ENTER to test lander...")
+num_episodes = 10
+num_steps = env.spec.max_episode_steps
+ep_rets, ave_rets = [], []
+for ep in range(num_episodes):
+    obs, done, rewards = env.reset(), False, []
+    for st in range(num_steps):
+        env.render()
+        a = tf.stop_gradient(tf.squeeze(actor_net(obs.reshape(1,-1))))
+        a += act_noise * np.random.randn(act_dim)
+        act = np.clip(a, -act_limit, act_limit)
+        next_obs, rew, done, info = env.step(act)
+        rewards.append(rew)
+        print("\n-\nepisode: {}, step: {} \naction: {} \nobs: {}, \nreward: {}".format(ep+1, st+1, act, obs, rew))
+        obs = next_obs.copy()
+        if done:
+            ep_rets.append(sum(rewards))
+            ave_rets.append(sum(ep_rets)/len(ep_rets))
+            print("\n---\nepisode: {} \nepisode return: {}, averaged return: {} \n---\n".format(ep+1, ep_rets[-1], ave_rets[-1]))
+            break
 
 
 
