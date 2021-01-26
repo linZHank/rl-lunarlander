9f86bc6e219625c4795deec6ebb2792e0b63112f
linzhank <linzhank@gmail.com>
2021-01-25 19:07:11 -0500

    fixing new agent: shapes

--- agents/ppo.py
+++ agents/ppo.py
@@ -3,11 +3,11 @@ A PPO type agent class for LunarLander env
 """
 import tensorflow as tf
 import numpy as np
+import scipy.signal
 import logging
-import pdb
 
 
-################################################################
+#############################Setup##############################
 """
 Can safely ignore this block
 """
@@ -31,9 +31,92 @@ if gpus:
         print(e)
 # set log level
 logging.basicConfig(format='%(asctime)s %(message)s',level=logging.DEBUG)
-################################################################
+#############################Setup##############################
 
 
+#############################Buffer#############################
+"""
+On-policy Replay Buffer 
+"""
+def discount_cumsum(x, discount):
+    """
+    magic from rllab for computing discounted cumulative sums of vectors.
+    input:
+        vector x, [x0, x1, x2]
+    output:
+        [x0+discount*x1+discount^2*x2, x1+discount*x2, x2]
+    """
+    return scipy.signal.lfilter([1], [1, float(-discount)], x[::-1], axis=0)[::-1]
+
+class PPOBuffer: 
+    """
+    A buffer for storing trajectories experienced by a PPO agent interacting
+    with the environment, and using Generalized Advantage Estimation (GAE-Lambda)
+    for calculating the advantages of state-action pairs.
+    """
+
+    def __init__(self, dim_obs, dim_act, max_size, gamma=0.99, lam=0.95):
+        self.obs_buf = np.zeros((max_size, dim_obs), dtype=np.float32)
+        self.act_buf = np.zeros((max_size, dim_act), dtype=np.float32)
+        self.adv_buf = np.zeros((max_size,1), dtype=np.float32)
+        self.rew_buf = np.zeros((max_size,1), dtype=np.float32)
+        self.ret_buf = np.zeros((max_size,1), dtype=np.float32)
+        self.val_buf = np.zeros((max_size,1), dtype=np.float32)
+        self.logp_buf = np.zeros((max_size,1), dtype=np.float32)
+        # params
+        self.dim_obs=dim_obs
+        self.dim_act=dim_act
+        self.gamma=gamma
+        self.lam=lam
+        self.max_size=max_size 
+        # vars
+        self.ptr, self.path_start_idx, = 0, 0
+
+    def store(self, obs, act, rew, val, logp):
+        """
+        Append one timestep of agent-environment interaction to the buffer.
+        """
+        assert self.ptr <= self.max_size     # buffer has to have room so you can store
+        self.obs_buf[self.ptr] = obs
+        self.act_buf[self.ptr] = act
+        self.rew_buf[self.ptr] = rew
+        self.val_buf[self.ptr] = val
+        self.logp_buf[self.ptr] = logp
+        self.ptr += 1
+
+    def finish_path(self, last_val=0):
+        """
+        Call this at the end of a trajectory, to compute advantage estimates with GAE-Lambda, as well as compute the rewards-to-go.
+        The "last_val" argument should be 0 if the trajectory ended, and otherwise should be V(s_T).
+        """
+        path_slice = slice(self.path_start_idx, self.ptr)
+        rews = np.append(self.rew_buf[path_slice], last_val)
+        vals = np.append(self.val_buf[path_slice], last_val)
+        # the next two lines implement GAE-Lambda advantage calculation
+        deltas = rews[:-1] + self.gamma * vals[1:] - vals[:-1]
+        self.adv_buf[path_slice] = discount_cumsum(deltas, self.gamma * self.lam)
+        # the next line computes rewards-to-go, to be targets for the value function
+        self.ret_buf[path_slice] = discount_cumsum(rews, self.gamma)[:-1]
+        self.path_start_idx = self.ptr
+
+    def get(self):
+        """
+        Call this at the end of an epoch to get all of the data from the buffer, with advantages normalized. Also, resets the buffer.
+        """
+        assert self.ptr<=self.max_size    # buffer has to be full before you can get
+        # the next two lines implement the advantage normalization trick
+        adv_mean = np.mean(self.adv_buf)
+        adv_std = np.std(self.adv_buf)
+        self.adv_buf = (self.adv_buf - adv_mean) / adv_std
+        data = dict(obs=self.obs_buf, act=self.act_buf, ret=self.ret_buf,
+                    adv=self.adv_buf, logp=self.logp_buf)
+        # reset buffer
+        self.__init__(self.dim_obs, self.dim_act, self.max_size)
+        return {k: tf.convert_to_tensor(v, dtype=tf.float32) for k,v in data.items()}
+#############################Buffer#############################
+
+
+#############################Agent##############################
 class CategoricalActor(tf.keras.Model):
 
     def __init__(self, dim_obs, num_act, **kwargs):
@@ -49,46 +132,25 @@ class CategoricalActor(tf.keras.Model):
             ]
         )
 
-
     @tf.function
-    def logprob(self, obs, act):
-        pmf = self.call(obs) # PMF
-        act_oh = tf.squeeze(tf.one_hot(tf.cast(act,tf.int32), self.num_act))
-        p_a = tf.math.reduce_sum(tf.math.multiply(pmf, act_oh), axis=-1)
-        logp_a = tf.squeeze(tf.math.log(p_a))
-        return pmf, logp_a
-
-    @tf.function
-    def call(self, obs):
+    def _distribution(self, obs):
         pmf = self.policy_net(obs)
         return pmf
 
-# class GaussianActor(tf.Module):
-#     def __init__(self, dim_obs, dim_act):
-#         super().__init__()
-#         self.log_std = tf.Variable(initial_value=-0.5*np.ones(dim_act, dtype=np.float32))
-#         self.mu_net = mlp(dim_inputs=dim_obs, dim_outputs=dim_act, activation='relu')
-# 
-#     def _distribution(self, obs):
-#         mu = tf.squeeze(self.mu_net(obs))
-#         std = tf.math.exp(self.log_std)
-# 
-#         return tfd.Normal(loc=mu, scale=std)
-# 
-#     def _log_prob_from_distribution(self, pi, act):
-#         return tf.math.reduce_sum(pi.log_prob(act), axis=-1)
-# 
-#     def call(self, obs, act=None):
-#         # def log_normal_pdf(sample, mean, log_std, raxis=1):
-#         #     log2pi = tf.math.log(2.*np.pi)
-#         #     return tf.reduce_sum(-.5*(((sample-mean)*tf.math.exp(-log_std))**2 + 2*log_std + log2pi), axis=raxis)
-#         pi = self._distribution(obs)
-#         logp_a = None
-#         if act is not None:
-#             logp_a = self._log_prob_from_distribution(pi, act)
-#             
-# 
-#         return pi, logp_a
+    @tf.function
+    def _logprob(self, pmf, act):
+        act_oh = tf.one_hot(tf.cast(act,tf.int32), self.num_act)
+        p_a = tf.math.reduce_sum(pmf*act_oh, axis=-1)
+        logp_a = tf.math.log(p_a)
+        return logp_a
+        
+    @tf.function
+    def call(self, obs, act=None):
+        pmf = self._distribution(obs)
+        logp_a = None
+        if act is not None:
+            logp_a = self._logprob(pmf, act)
+        return pmf, logp_a
 
 class Critic(tf.keras.Model):
     def __init__(self, dim_obs, **kwargs):
@@ -107,14 +169,14 @@ class Critic(tf.keras.Model):
         return self.value_net(obs)
 
 class PPOAgent:
-    def __init__(self, name='ppo_agent', continuous=False, dim_obs=8, num_act=4, dim_act=1, clip_ratio=0.2, lr_actor=1e-4, lr_critic=1e-3, target_kl=0.01, beta=0.001):
+    def __init__(self, name='ppo_agent', continuous=False, dim_obs=8, num_act=4, dim_act=1, clip_ratio=0.2, lr_actor=1e-4, lr_critic=1e-3, target_kld=0.2, beta=0.001):
         # params
         self.continuous=continuous
         self.clip_ratio = clip_ratio
         self.dim_obs = dim_obs
         self.dim_act = dim_act
         self.beta = beta
-        self.target_kl = target_kl
+        self.target_kld = target_kld
         # modules
         self.actor = CategoricalActor(dim_obs, num_act)
         self.critic = Critic(dim_obs)
@@ -122,10 +184,10 @@ class PPOAgent:
         self.critic_optimizer = tf.keras.optimizers.Adam(learning_rate=lr_critic)
 
     def make_decision(self, obs):
-        prob = self.actor(obs)
-        act = tf.squeeze(tf.random.categorical(logits=prob, num_samples=1))
-        logp_a = tf.squeeze(self.actor.logprob(obs,act))
-        val = tf.squeeze(self.critic(obs))
+        pmf = self.actor._distribution(obs)
+        act = tf.squeeze(tf.random.categorical(logits=pmf, num_samples=1))
+        logp_a = self.actor._logprob(pmf,act)
+        val = self.critic(obs)
 
         return act.numpy(), val.numpy(), logp_a.numpy()
 
@@ -147,7 +209,7 @@ class PPOAgent:
             ep_ent = tf.convert_to_tensor([]) # entropy storage
             with tf.GradientTape() as tape:
                 tape.watch(self.actor.trainable_variables)
-                pi, logp = self.actor.logprob(data['obs'], data['act'])
+                pi, logp = self.actor(data['obs'], data['act'])
                 ratio = tf.math.exp(logp - data['logp']) # pi/old_pi
                 clip_adv = tf.math.multiply(tf.clip_by_value(ratio, 1-self.clip_ratio, 1+self.clip_ratio), data['adv'])
                 approx_kld = data['logp'] - logp
@@ -171,8 +233,8 @@ class PPOAgent:
                 mean_kld
             ))
             # early cutoff due to large kl-divergence
-            if mean_kld > self.target_kl:
-                logging.warning("Early stopping at epoch {} due to reaching max kl-divergence.".format(ep+1))
+            if mean_kld > self.target_kld:
+                logging.warning("\nEarly stopping at epoch {} due to reaching max kl-divergence.\n".format(ep+1))
                 break
         # update critic
         for ep in range(epochs):
@@ -193,6 +255,7 @@ class PPOAgent:
             ))
 
         return mean_loss_pi, mean_loss_val, dict(kld=mean_kld, entropy=mean_ent)
+#############################Agent##############################
         
 
 # Uncomment following for testing the PPO agent
